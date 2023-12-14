715cef13742973d34dc58967c997582628a2032c
DiesDxo <121221077+DiesDxo@users.noreply.github.com>
2023-12-15 05:39:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN'; // Replace with your Telegram bot token
+const TELEGRAM_TOKEN = '6983020883:AAGnpOrwAskwL4VfuoiJI6iCpCBxbi4Glb8'; // Replace with your Telegram bot token
 
 module.exports = {
   TELEGRAM_TOKEN,
